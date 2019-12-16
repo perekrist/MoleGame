f1cac676cfa1daaed5cd5cd1140a27281ab16083
Kristina Peregudova <perekrist31@gmail.com>
2019-12-16 17:02:32 +0700

    add tomer

--- SimpleApp/ViewController.swift
+++ SimpleApp/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var points: UILabel!
+    @IBOutlet weak var timer: UILabel!
+    
+    var pts = 0
+    var seconds = 60
+    var time = Timer()
+    var isTimerRunning = false
+
+    @IBOutlet weak var start: NSLayoutConstraint!
+    @IBAction func StartBtn(_ sender: Any) {
+        runTimer()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    func runTimer() {
+         time = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        seconds -= 1
+        if seconds <= 0 {
+            seconds = 0
+        }
+        timer.text = "\(seconds)"
     }
 
 
