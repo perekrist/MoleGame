43f8c26726c3f076d4b94d15ddfb405a9c13fedb
Kristina Peregudova <perekrist31@gmail.com>
2019-12-16 16:05:09 +0700

    Initial Commit

---
+++ SimpleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleApp
+//
+//  Created by Перегудова Кристина on 16/12/2019.
+//  Copyright © 2019 perekrist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
