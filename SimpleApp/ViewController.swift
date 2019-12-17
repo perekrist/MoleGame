76672deafff2cda052638a0813dcf7eb362fb169
Kristina Peregudova <perekrist31@gmail.com>
2019-12-18 01:00:40 +0700

    finished

--- SimpleApp/ViewController.swift
+++ SimpleApp/ViewController.swift
@@ -7,15 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     var screenHeight = UIScreen.main.bounds.height,
     screenWidth = UIScreen.main.bounds.width
+    
+    var audioPlayer = AVAudioPlayer()
+    var soundPlayer = AVAudioPlayer()
 
     @IBOutlet weak var points: UILabel!
+    @IBOutlet weak var best: UILabel!
     @IBOutlet weak var timer: UILabel!
     
-    var pts = 0
+    var score = 0
+    var bestScore = 0
     var seconds = 10
     var time = Timer()
     var moleTime = Timer()
@@ -25,6 +31,20 @@ class ViewController: UIViewController {
     var timeInt = 1.0
     var index = 1
     var mole = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let song = Bundle.main.path(forResource: "song", ofType: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: song!))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
+        } catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
+    
     @IBAction func StartBtn(_ sender: Any) {
         let alert = UIAlertController(title: "Set configurations", message: nil, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
@@ -52,9 +72,6 @@ class ViewController: UIViewController {
         }))
         self.present(alert, animated: true)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
     func runTimer() {
          time = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
@@ -69,15 +86,15 @@ class ViewController: UIViewController {
         if seconds <= 0 {
             seconds = 0
             moleTime.invalidate()
-            let alert = UIAlertController(title: "Time over", message: "Your score \(pts) pts.", preferredStyle: .alert)
+            let alert = UIAlertController(title: "Time over", message: "Your score \(score) pts.", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Restart", style: .default, handler: {action in
                 self.restart()
                 }))
             self.present(alert, animated: true, completion: nil)
             time.invalidate()
         }
-        timer.text = "\(seconds)"
-        if pts < 0 {
+        timer.text = "Seconds: \(seconds)"
+        if score < 0 {
             moleTime.invalidate()
             time.invalidate()
             let alert = UIAlertController(title: "Game over", message: nil, preferredStyle: .alert)
@@ -117,26 +134,37 @@ class ViewController: UIViewController {
         mole = r
         let hole = self.view.viewWithTag(r) as? UIButton
         hole?.setImage(UIImage(named: "boxO")?.withRenderingMode(.alwaysOriginal), for: .normal)
-        hole?.setTitle(" ", for: .normal)
     }
     
     @objc func buttonClicked(sender : UIButton){
+        var sound = Bundle.main.path(forResource: "good", ofType: "mp3")
         if sender.tag == mole {
-            pts += 1
+            score += 1
             sender.setImage(UIImage(named: "boxC")?.withRenderingMode(.alwaysOriginal), for: .normal)
         } else {
-            pts -= 1
+            sound = Bundle.main.path(forResource: "bad", ofType: "mp3")
+            score -= 1
         }
-        points.text = "\(pts) pts."
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+            soundPlayer.play()
+        } catch {
+            print(error)
+        }
+        points.text = "Score: \(score) pts."
     }
         
     func restart() {
         time.invalidate()
         moleTime.invalidate()
-        pts = 0
-        points.text = "\(self.pts) pts."
-        seconds = 60
-        timer.text = "\(self.seconds)"
+        if score > bestScore {
+            bestScore = score
+        }
+        score = 0
+        points.text = "Score: \(self.score) pts."
+        seconds = 10
+        timer.text = "Seconds: \(self.seconds)"
+        best.text = "Best: \(bestScore) pts."
         runTimer()
         runMoleTimer()
         for i in 1...index {
@@ -153,4 +181,5 @@ class ViewController: UIViewController {
             button?.setImage(UIImage(named: "boxC")?.withRenderingMode(.alwaysOriginal), for: .normal)
         }
     }
+
 }
